4ece1e9fd3b55eb7abcd6a34100bed20436859c9
C. Corey Capel <corey@capelio.com>
2015-07-03 11:31:20 +0545

    Generate a 200.html for deep linking on surge.sh

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,12 @@ var getConfig = require('hjs-webpack')
 module.exports = getConfig({
   in: 'src/app/app.js',
   out: 'public',
-  isDev: process.env.NODE_ENV !== 'production'
+  isDev: process.env.NODE_ENV !== 'production',
+
+  html: function (context) {
+    return {
+      '200.html': context.defaultTemplate(),
+      'index.html': context.defaultTemplate()
+    }
+  }
 })
