690b1ab82509d9a6e17ec75dc3ceff2b7c101809
C. Corey Capel <corey@capelio.com>
2015-07-04 18:25:17 +0545

    Make several changes to the student details page
    Changes:
    - Get isPartner status from app, don't expect it as a prop. This comes
      from our convention to use pages to compose components.
    - Use a canEdit variable to determine which content to render. Using
      isPartner reads too specifically to the implementation.
    - Add missing propTypes

--- src/pages/student-details.js
+++ src/pages/student-details.js
@@ -1,16 +1,25 @@
+import app from 'ampersand-app'
 import React from 'react'
 import ampersandMixin from 'ampersand-react-mixin'
-import StudentDetails from 'components/student-details'
+
 import StudentAdminBar from 'components/student-admin-bar'
+import StudentDetails from 'components/student-details'
+import types from 'helpers/prop-types'
 
 export default React.createClass({
   mixins: [ampersandMixin],
 
+  propTypes: {
+    student: types.student
+  },
+
   render () {
-    const {student, isPartner} = this.props
+    const {student} = this.props
+    const canEdit = app.me.isPartner
+
     let content
 
-    if (isPartner) {
+    if (canEdit) {
       content = (
         <div className='student-details-page'>
           <StudentDetails student={student}/>
