099d469ee86b9bd7eda8ee2b120838ab1b135da3
C. Corey Capel <corey@capelio.com>
2015-07-04 18:12:48 +0545

    Make several changes to the funding progress component
    Changes
    - Include the ampersand react mixin
    - Use models as React props so our mixin knows when to trigger a re-render
    - Use canDonate instead of allowDonations for naming consistency

--- src/components/funding-progress.js
+++ src/components/funding-progress.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import types from 'helpers/prop-types'
+import ampersandMixin from 'ampersand-react-mixin'
 
-const {bool, number} = React.PropTypes
+const {bool, object} = React.PropTypes
 
 export default React.createClass({
+  mixins: [ampersandMixin],
+
   propTypes: {
-    allowDonations: bool,
-    goal: number,
-    raised: number,
-    student: types.student
+    canDonate: bool,
+    donations: object,
+    student: object
   },
 
   render () {
-    const {allowDonations, goal, raised, student} = this.props
+    const {canDonate, donations, student} = this.props
+    const {donateUrl, goal} = student
+
+    const startTotalAt = 0
+    const raised = donations.reduce((total, d) => total + d.amount, startTotalAt)
     const toGo = goal - raised
 
     return (
@@ -21,8 +26,8 @@ export default React.createClass({
           <span>${raised} raised, ${toGo} to go</span>
         </div>
 
-        <div className='grid-flex-cell funding-progress_donate' style={{display: allowDonations ? 'block' : 'none', textAlign: 'right'}}>
-          <a href={student.donateUrl} className='button'>Donate</a>
+        <div className='grid-flex-cell funding-progress_donate' style={{display: canDonate ? 'block' : 'none', textAlign: 'right'}}>
+          <a href={donateUrl} className='button'>Donate</a>
         </div>
       </div>
     )
