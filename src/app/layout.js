3a213aea638399916be52e71690b346a50077585
C. Corey Capel <corey@capelio.com>
2015-07-06 23:25:00 +0545

    Change name of Students page link to Students in Need

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default React.createClass({
 
           <ul className='list-unstyled list-inline cf'>
             <li>Empower Nepal</li>
-            <li><a href='/'>Students</a></li>
+            <li><a href='/'>Students in Need</a></li>
             <li><a href='/partners'>Partners</a></li>
             <li><a href='/teachers'>Teachers</a></li>
           </ul>
